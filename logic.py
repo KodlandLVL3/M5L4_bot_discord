5156b740b7c83c58fa8edcaae112b6363fe30285
Kootyra <110038102+Kootyra@users.noreply.github.com>
2024-09-06 13:27:34 +0200

    first commit

--- logic.py
+++ logic.py
@@ -6,17 +6,13 @@ def process_image(image_path, output_path):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     # Загрузка изображения
-    image = cv2.imread(image_path)
-
-    if image is None:
-        print("Ошибка: Не удалось загрузить изображение.")
-        return
+    image = 
 
     # Преобразование изображения в оттенки серого для улучшения производительности детектора
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = 
 
     # Обнаружение лиц в изображении
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = 
 
     # Размытие области вокруг каждого обнаруженного лица
     for (x, y, w, h) in faces:
@@ -24,14 +20,13 @@ def process_image(image_path, output_path):
         face_region = image[y:y+h, x:x+w]
 
         # Применение размытия
-        blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
+        blurred_face = 
 
         # Замена области лица размытым изображением
         image[y:y+h, x:x+w] = blurred_face
 
     # Сохранение обработанного изображения
-    cv2.imwrite(output_path, image)
-    print(f"Изображение сохранено в {output_path}")
+    
 
 
 # Проверяем работу кода
